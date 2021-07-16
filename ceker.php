8a68d4f1a7f939778a263865637333b939d12985
MR.XXII <betazona1984@gmail.com>
2021-07-17 00:06:46 +0900

    Create ceker.php

--- ceker.php
+++ ceker.php
@@ -1,5 +1,5 @@
 <?php 
 print "[ Aplikasi check No.KTP & KK ]\n\n";
  print "Link download nya : https://goo.gl/vqbvtA\n\n";
-print "Thanks To: BabbyCyberTeam || SGB-TEAM || JNCK MEDIA || And You\n";
+print "Thanks To: ZONARED XXII TEAM || SERAM-TIMUR || MR.XXII || SECURITY CYBER\n";
 ?>
